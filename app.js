3ba5d6e269d1379bd9329a8a6fd7e77713662110
PiyushSuneja <84224866+PiyushSuneja@users.noreply.github.com>
2021-11-16 20:26:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const forecast = require('./forecast');
+const geocode = require('./geocode');
+
+const address = process.argv[2];
+
+if (!address) {
+    console.log('Please provide an address!');
+} else {
+    geocode(address, (err, response) => {
+        if (err) {
+            console.log(err);
+            return ;
+        }
+
+        let place_name = response.features[0].place_name;
+        let lat = response.features[0].center[1];
+        let long = response.features[0].center[0]
+        console.log(`The place is ${place_name}`);
+
+        forecast(lat, long, (err, response) => {
+            if (err) {
+                console.log(err);
+                return ;
+            }
+            console.log(response);
+        });
+
+    });
+}
+
+
+
